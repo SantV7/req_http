068e47a182e208f08e08dc32b5234ec97809ac80
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-19 01:43:37 -0300

    Requisição simples com o uso de usseefect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
+import { useEffect, useState } from "react"
 
 
 function App() {
+  
+  const [data, setData] = useState([])
+
+  const url = "http://localhost:3000/products"
+
+
+  async function requisicao() {
+    try {
+      const request = await fetch(url)
+
+      if(!request.ok) {
+        throw new Error(`Erro na requisição: ${request.status}`)
+      }
+
+      const response = await request.json()
+      setData(response)
+    }
+
+    catch (error) {
+      console.error(`Erro: ${error}`)
+    }
+ }
+  
+  useEffect(() => {
+    requisicao()
+  }, [])
+
+  console.log(data)
 
 
   return (
     <>
-    <h1>Requisições HTTP</h1>
+    <div className="appMain">
+      <h1>Lista de produtos</h1>
+
+      {data.map((itemList) => (
+        <div key={itemList.id}>
+          <p>Produto: {itemList.name} - R${itemList.price}</p>
+        </div>
+
+      ))}
+    </div>
 
     </>
   )
