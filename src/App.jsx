e287d12815cbe80f3407dc600f4c25fb092d9f54
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-07-06 22:59:09 -0300

    FIXING ERROR

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react"
-import '../global.css'
+import '../src/global.css'
 import useFetch from "./hooks/useFetch"
 
 function App() {
   
   const [ data, setData ] = useState([])
-
   const [ name, setName ] = useState("")
-
   const [ price, setPrice ] = useState("")  
   const [ warning, setWarning ] = useState(false)
 
   const url = "http://localhost:3000/products"
 
-  const { dataFetch, httpConfig,  isLoading } = useFetch(url)
+  const { dataFetch, httpConfig,  isLoading, errorRequest } = useFetch(url)
 
     useEffect(() => {
       if(dataFetch) {
@@ -21,48 +19,34 @@ function App() {
       }
     }, [dataFetch])
 
-    console.log(data)
 
     async function handleSubmit(event) {
       event.preventDefault()
 
+      //validação para os dois campos
       if(name.trim() === "" || price.trim() === "") {
        return alert("É necessário ter um nome e um preço")
       }
 
-      const idProducts = data.length > 0 ? String(Number(data[data.length - 1].id) + 1) : 1
-      // o tamanho de data é maior que 0 ? se sim, converta para string o numero em data[tamanho do index] acesse o último elemento da lista pegando o tamanho total menos 1, pegue o id dele, converte para número e adicione 1, se nao coloque 1
-
+      // obj que é enviado para o backend
       const products = {
-        id: idProducts,
         name,
-        price: parseFloat(price)
+        price: Number(price.replace(",", ".")).toFixed(2)
       }
-
+  
       httpConfig(products, "POST")
-
-      setData(dataFetch)
 
       setName("")
       setPrice("")
   }
 
+  
 
   return (
     <>
     <div className="appMain">
       <h1>Lista de produtos</h1>
-
-      {isLoading && <p>Carregando dados....</p>}
-  {!isLoading && data && (
-    <ul>
-      {data.map((itemList) => (
-        <li key={itemList.id}>
-          Produto: {itemList.name} - R${itemList.price}
-        </li>
-      ))}
-    </ul>
-  )}
+    
 
        <div className="add-product">
           <form onSubmit={handleSubmit}>
@@ -84,9 +68,26 @@ function App() {
                 onChange={(event) => setPrice(event.target.value)}/>
             </label>
 
-            <button type="submit" value='Criar'>Criar</button>
+            {isLoading ? <button type="submit" disabled value='Loading...'>Carregando...</button> : <button type="submit" value='Criar'>Criar</button>}
+      
+
           </form>
         </div> 
+        {isLoading && <p>Carregando dados...</p>}
+        {errorRequest && <>OPS! Algo deu errado, tente novamente.</>}
+        
+            {!isLoading && data && (
+              <ul>
+                {data.map((itemList) => (
+                  <li key={itemList.id}>
+                    Produto: {itemList.name} - R${itemList.price}
+                  </li>
+                ))}
+              </ul>
+              
+            )}
+
+            
         
     </div>
     </>
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from 'react'
 
 export const useFetch = (url) => {
+
   const [dataFetch, setDataFetch] = useState(null)
-
   const [config, setConfig] = useState(null)
-
   const [method, setMethod] = useState(null)
-  
   const [isLoading, setIsLoading] = useState(false)
-
+  const [ errorRequest, setErrorRequest ] = useState(null)
 
   
   const httpConfig = (bodyData, method) => {
         if(method === "POST") {
+          
           setConfig({
             method: "POST",
             headers: {
@@ -24,36 +23,12 @@ export const useFetch = (url) => {
         setMethod(method)
   }
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          setIsLoading(true)
-          const request = await fetch(url)
 
-          if(!request.ok) {
-            throw new Error(`Erro na requisição: ${request.status}`)
-          } 
-
-          const response = await request.json()
-
-          setDataFetch((prevData) => [...prevData, response])
-        } catch (err) {
-          console.error(`ERROR: ${err}`)
-        }
-        finally {
-          setIsLoading(false)
-        }
-      }
-
-      fetchData()
-    }, [url])
-
-
-
-    useEffect(() => {
+      useEffect(() => {
       const fetchPost = async () => {
         try {
           setIsLoading(true)
+          setErrorRequest(null)
           const request = await fetch(url, config)
   
           if(!request.ok) {
@@ -65,6 +40,7 @@ export const useFetch = (url) => {
           setDataFetch((prevData) => [...prevData, response])
         } catch (err) {
           console.error(`ERROR: ${err}`)
+          setErrorRequest(`Houve um erro ao carregar dados!`)
         }
         finally {
           setMethod(null)
@@ -76,11 +52,36 @@ export const useFetch = (url) => {
       if(method === "POST" && config) {
         fetchPost()
       }
-      // trava de segurando pra não executar se for GET
-      
-    }, [ url, config ] )
+    }, [ url, config, method ] )
+
+
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          setIsLoading(true)
+          setErrorRequest(null)
+          const request = await fetch(url)
+
+          if(!request.ok) {
+            throw new Error(`Erro na requisição: ${request.status}`)
+          } 
+
+          const response = await request.json()
+
+          setDataFetch(response)
+        } catch (err) {
+            console.error(`ERROR: ${err}`)
+            setErrorRequest(`Houve um erro ao carregar dados!`)
+        } finally {
+            setIsLoading(false)
+        }
+      } 
+
+      fetchData()
+    }, [url])
  
-  return { dataFetch, httpConfig,  isLoading };
+  return { dataFetch, httpConfig,  isLoading, errorRequest };
 }
 
 export default useFetch
